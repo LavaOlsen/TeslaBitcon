095448cb59159e47ae8744fcc87ab999fb7687de
Jonas Ringive Korsholm <jonas.k345@gmail.com>
2021-02-27 11:44:49 +0100

    Create TwitterAPI.R

---
+++ TwitterAPI.R
@@ -0,0 +1,12 @@
+
+API_key <- "e945SfhIPWtfDUkKq3kpKmePd"
+
+API_secret_key <- "dRCz9bgSKEvp9KWNXwGNm8xcd3feo6nK9jFPvaqKgktrVg6cph"
+
+Bearer_token <- "AAAAAAAAAAAAAAAAAAAAAJW9NAEAAAAAAvHW2IdqvKHKQKSkm037%2Bw%2F7C9Y%3DWYdOFLvOElgzf9l8PL7UJpshy7pBTrrE1jogOcHZhukwicIpjb"
+
+Access_token <- "1365599852168634371-JJNktZtcy4S59yscZYOz5R8ABaU21h"
+
+Access_token_secret <- "g2Ga1hLqHjU1o2aejUb8mUTuKAVQSTXodxMNVZCKI1X1X"
+
+
